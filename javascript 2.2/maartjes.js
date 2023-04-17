2980776320f492fcc5ff0ea32068ccd25ee64d74
Debby <124309163+deborah-boat@users.noreply.github.com>
2023-04-17 22:18:56 +0200

    Add files via upload

---
+++ javascript 2.2/maartjes.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const monday = [
+  {
+    name: 'Write a summary HTML/CSS',
+    duration: 180
+  },
+  {
+    name: 'Some web development',
+    duration: 120
+  },
+  {
+    name: 'Fix homework for class10',
+    duration: 20
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 1.0
+  }
+];
+
+const tuesday = [
+  {
+    name: 'Keep writing summary',
+    duration: 1.0
+  },
+  {
+    name: 'Some more web development',
+    duration: 180
+  },
+  {
+    name: 'Staring out the window',
+    duration: 10
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 1.0
+  },
+  {
+    name: 'Look at application assignments new students',
+    duration: 40
+  }
+];
+
+
+const maartjesTasks = monday.concat(tuesday);
+const maartjesHourlyRate = 20;
+
+function computeEarnings(tasks, hourlyRate) {
+  const tasksInHours = tasks.map((task) => task.duration / 60);
+  const filteredTasks = tasksInHours.filter((duration) => duration >= 2);
+
+  const calculatedEarnings = filteredTasks.reduce(
+    (sum, duration) => sum + duration * hourlyRate,
+    0,
+  );
+  return calculatedEarnings;
+}
+
+// eslint-disable-next-line no-unused-vars
+const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
+const formattedEarnings = earnings.toFixed(2);
+
+console.log(`Maartje has earned €${formattedEarnings}`);
+
+// Do not change or remove anything below this line
+module.exports = {
+  maartjesTasks,
+  maartjesHourlyRate,
+  computeEarnings,
+};
+
+
+
+
+
+
+
+
+
---
+++ javascript 2.2/map-filter.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+function doubleOddNumbers(numbers) {
+  return number.filter(num => num % 2 !== 0).map(num => num * 2);
+}
+
+const myNumbers = [1,2,3,4];
+console.log(doubleOddNumbers)
+
+// Do not change or remove anything below this line
+module.exports = {
+  myNumbers,
+  doubleOddNumbers,
+};
+
+
+
+
+
+
+
