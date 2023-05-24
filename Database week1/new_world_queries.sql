524d45c827e7ff29b7f682f84d5f96678bb40449
Debby <124309163+deborah-boat@users.noreply.github.com>
2023-05-24 23:02:56 +0200

    Update new_world_queries.sql

--- Database week1/new_world_queries.sql
+++ Database week1/new_world_queries.sql
@@ -55,17 +55,25 @@ SELECT country.name
 FROM country
 INNER JOIN city
 ON country.code = city.countrycode
-GROUP BY countrycode HAVING COUNT(*) > 10 AND SUM(city.population) > 50000000;
+GROUP BY countrycode HAVING COUNT(*) > 10 AND SUM(city.population) > 50000000
+ORDER BY total_city_population ASC;
+
 
 
 --->EXTRA EXTRA: List the cities from those countries, where the city population is > 5M.
 
-SELECT city.name, country.name, population
-FROM cities
-WHERE population > 5000000;
-
-
-
+SELECT city.name, city.population
+FROM country
+JOIN city ON country.code = city.countrycode
+WHERE country.name IN (
+  SELECT country.name
+  FROM country
+  JOIN city ON country.code = city.countrycode
+  GROUP BY country.name
+  HAVING COUNT(city.name) > 10 AND SUM(city.population) > 50000000
+)
+AND city.population > 5000000
+ORDER BY city.population ASC;
 
 
 
