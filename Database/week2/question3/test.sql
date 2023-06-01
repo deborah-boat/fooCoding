6ab10b97e2e9cd399bb128bf3e47c357c634ac15
Debby <124309163+deborah-boat@users.noreply.github.com>
2023-06-01 10:54:37 +0200

    Update test.sql

--- Database/week2/question3/test.sql
+++ Database/week2/question3/test.sql
@@ -1,14 +1,14 @@
  INSERT INTO countrylanguage (CountryCode, Language)
-     VALUES ('GHA', 'Language1'),
-            ('GHA', 'Language2'),
-            ('GHA', 'Language3'),
-            ('GHA', 'Language4'),
-           ('GHA', 'Language5'),
-           ('GHA', 'Language6'),
-            ('GHA', 'Language7'),
-            ('GHA', 'Language8'),
-            ('GHA', 'Language9'),
-            ('GHA', 'Language10');
+     VALUES ('GHA', 'English'),
+            ('GHA', 'Twi'),
+            ('GHA', 'English'),
+            ('GHA', 'English'),
+           ('GHA', 'Twi'),
+           ('GHA', 'English'),
+            ('GHA', 'English'),
+            ('GHA', 'Twi'),
+            ('GHA', 'English'),
+            ('GHA', 'Fante');
             
             ---Error alert popup when insert  a country---
 	//ERROR 1644 (45000): Total number of languages equals or exceeds 10
