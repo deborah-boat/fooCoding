d0461343d9418a3f2f0052e95854622582a9562e
Debby <124309163+deborah-boat@users.noreply.github.com>
2023-06-09 17:06:36 +0200

    Update newtodo_db.sql

--- Database week3/newtodo_db.sql
+++ Database week3/newtodo_db.sql
@@ -20,6 +20,20 @@ CREATE TABLE `todo_database`.`users` (
   `username` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`user_id`));
   
+  CREATE TABLE Item_Tags (
+  `item_id` INT NOT NULL,
+  `tag_id` INT NOT NULL,
+  FOREIGN KEY (`item_id`) REFERENCES Items(`id`),
+  FOREIGN KEY (`tag_id`) REFERENCES Tags(`id`)
+);
+
+  CREATE TABLE Reminders (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  `list_id` INT NOT NULL,
+  reminder_date DATE NOT NULL,
+  FOREIGN KEY (`list_id`) REFERENCES TodoLists(`id`)
+);
+  
 ALTER TABLE `todo_database`.`todos` 
 ADD CONSTRAINT `user_id`
   FOREIGN KEY (`user_id`)
@@ -40,4 +54,15 @@ INSERT INTO `todo_database`.`list_items` (`list_id`, `item`, `completed`) VALUES
 INSERT INTO `todo_database`.`list_items` (`list_id`, `item`, `completed`) VALUES ('1', 'milk', '0');
 INSERT INTO `todo_database`.`list_items` (`list_id`, `item`, `completed`) VALUES ('2', 'sweep', '1');
 INSERT INTO `todo_database`.`list_items` (`list_id`, `item`, `completed`) VALUES ('2', 'vacuum', '0');
+INSERT INTO `todo_database`.`tag_items` (`tag_id`, `item_id`) VALUES ('2', '1');
+INSERT INTO `todo_database`.`tag_items` (`tag_id`, `item_id`) VALUES ('1', '3');
+INSERT INTO `todo_database`.`tag_items` (`tag_id`, `item_id`) VALUES ('2', '3');
+INSERT INTO `todo_database`.`listreminder` (`date`, `list_id`) VALUES ('2023-19-14', '1');
+INSERT INTO `todo_database`.`listreminder` (`date`, `list_id`) VALUES ('2023-10-10', '2');
+INSERT INTO `todo_database`.`listreminder` (`date`, `list_id`) VALUES ('2023-01-09', '3');
+
+
+
+
+
 
